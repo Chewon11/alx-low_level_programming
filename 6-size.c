e35f82d575cbc23da799f2edf21875f3d5ca0cb9
Chewon11 <chowon360@yahoo.com>
2023-06-15 20:52:07 -0700

    6-size.c

--- 6-size.c
+++ 6-size.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+
 /**
- * main - Entry point
+ * main - Entry point c
  *
  * Return Always 0 (Success)
  */
 int main(void)
-{	
+{
 	char a;
 	int b;
 	long int c;
@@ -16,5 +17,6 @@ printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
 printf("Size of a long int: %lu bytes(s)\n", (unsigned long)sizeof(c));
 printf("Size of a long long int: %lu bytes(s)\n", (unsigned long)sizeof(d));
 printf("Size of a float: %lu bytes(s)\n", (unsigned long)sizeof(f));
+
 return (0);
 }
