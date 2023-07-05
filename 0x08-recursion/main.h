4377680e1bfa136ed74a176af4b3f32f3f298d6a
chewon11 <chowon360@yahoo.com>
2023-07-05 19:49:21 +0000

    main.h

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+_puts_recursion(char *s);
+_print_rev_recursion(char *s);
+_strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+
+#endif
