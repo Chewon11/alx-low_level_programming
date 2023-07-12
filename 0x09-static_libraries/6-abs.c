188752a12293daeae4bfc70eea405c8f08a89e34
chewon11 <chowon360@yahoo.com>
2023-07-12 19:39:14 +0000

    my tasks

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - vomputes the absolute value of an integer
+ * @n: the input to check
+ * Return: the absolute value of int
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
